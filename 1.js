6520f0651b1da1a28e6b9b1a7bd708628ae276fe
Mehasawa <35897012+Mehasawa@users.noreply.github.com>
2023-02-21 17:14:25 +0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,23 @@
+function f1(){
+
+	var pic1 = document.getElementById('pic1');
+	pic1.src="333.jpg";
+
+}
+function f2(){
+
+	var pic1 = document.getElementById('pic2');
+	pic1.src="3434343.jpg";
+
+}
+
+
+function p1(){
+	var pic1 = document.getElementById('gif');
+	pic1.src="gif2.gif";
+}
+
+function p2(){
+	var pic1 = document.getElementById('gif');
+	pic1.src="gif3.gif";
+}
